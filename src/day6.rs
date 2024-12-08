0d4e0ca2c5b61576261b1ce852507bbe6ae5d29e
Levente Krizsán <lewalevi@gmail.com>
2024-12-08 15:23:03 +0100

    feat(day6): solve second assignment

--- src/day6.rs
+++ src/day6.rs
@@ -19,7 +19,7 @@ impl std::fmt::Display for Tile {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 enum Direction {
     Up,
     Right,
@@ -41,9 +41,9 @@ impl Direction {
 #[derive(Debug)]
 struct Map {
     tiles: Array2D<Tile>,
+    starting_position: Coords2D,
     guard_position: Coords2D,
     direction: Direction,
-    visited_coords: HashSet<Coords2D>,
 }
 
 impl TryFrom<&str> for Map {
@@ -79,15 +79,20 @@ impl TryFrom<&str> for Map {
         let guard_position = Coords2D::new(guard_row_idx, guard_col_idx);
         Ok(Self {
             tiles,
+            starting_position: guard_position,
             guard_position,
             direction: Direction::Up,
-            visited_coords: HashSet::from([guard_position]),
         })
     }
 }
 
 impl Map {
-    fn tick(&mut self) -> bool {
+    fn reset(&mut self) {
+        self.direction = Direction::Up;
+        self.guard_position = self.starting_position;
+    }
+
+    fn step<F: FnMut(Coords2D, Direction) -> bool>(&mut self, mut callback: F) -> bool {
         let next_pos = match self.direction {
             Direction::Up => {
                 if self.guard_position.row == 0 {
@@ -121,18 +126,79 @@ impl Map {
             .expect("Should be a valid index since we checked it above");
         if let Tile::Obstacle = tile {
             self.direction = self.direction.turn();
-            self.tick()
+            self.step(callback)
         } else {
-            self.visited_coords.insert(next_pos);
+            if callback(next_pos, self.direction) {
+                return false;
+            }
             self.guard_position = next_pos;
             true
         }
     }
 
-    fn count_number_of_unique_visited_tiles(&mut self) -> usize {
-        while self.tick() {}
+    fn simulate_and_get_unique_visited_tiles(&mut self) -> HashSet<Coords2D> {
+        let mut visited_tiles = HashSet::from([self.guard_position]);
 
-        self.visited_coords.len()
+        while self.step(|coords2d: Coords2D, _| {
+            visited_tiles.insert(coords2d);
+            false
+        }) {}
+
+        visited_tiles
+    }
+
+    fn count_number_of_unique_visited_tiles(&mut self) -> usize {
+        self.simulate_and_get_unique_visited_tiles().len()
+    }
+
+    fn with_additional_obstacle(&self, new_obstacle_pos: Coords2D) -> Self {
+        let mut new_tiles = self.tiles.clone();
+        new_tiles
+            .set(
+                new_obstacle_pos.row,
+                new_obstacle_pos.column,
+                Tile::Obstacle,
+            )
+            .expect("Should be valid coords since we got it from `tiles");
+        Self {
+            starting_position: self.starting_position,
+            guard_position: self.starting_position,
+            tiles: new_tiles,
+            direction: Direction::Up,
+        }
+    }
+
+    fn simulate_and_check_if_cycle(&mut self) -> bool {
+        let mut unique_tiles_and_dirs = HashSet::from([(self.guard_position, self.direction)]);
+        let mut is_cycle = false;
+
+        while self.step(|coords, direction| {
+            let is_new_tile = unique_tiles_and_dirs.insert((coords, direction));
+            is_cycle = !is_new_tile;
+            is_cycle
+        }) {}
+
+        is_cycle
+    }
+
+    fn count_number_of_added_obstacles_that_result_in_cycle(&mut self) -> usize {
+        let unique_tiles = self.simulate_and_get_unique_visited_tiles();
+        let mut cnt = 0;
+
+        for potential_obstacle_tile in unique_tiles {
+            if potential_obstacle_tile == self.starting_position {
+                // Cannot place new obstacle on guard's starting position
+                continue;
+            }
+
+            let mut new_map = self.with_additional_obstacle(potential_obstacle_tile);
+            let does_result_in_cycle = new_map.simulate_and_check_if_cycle();
+            if does_result_in_cycle {
+                cnt += 1;
+            }
+        }
+
+        cnt
     }
 }
 
@@ -141,5 +207,11 @@ fn main() {
     let mut map = Map::try_from(input.as_str()).unwrap();
 
     let number_of_unique_visited_tiles = map.count_number_of_unique_visited_tiles();
-    println!("The number of unique tiles the guard visits is {number_of_unique_visited_tiles}")
+    println!("The number of unique tiles the guard visits is {number_of_unique_visited_tiles}");
+
+    map.reset();
+
+    let number_of_obstacles_that_cause_a_cycle =
+        map.count_number_of_added_obstacles_that_result_in_cycle();
+    println!("The number of obstacles we can place that causes a cycle is {number_of_obstacles_that_cause_a_cycle}");
 }
